0df93a85beb3559aace694fd4a85c233f27517dd
italokkDev <dev.italoramon@gmail.com>
2024-11-17 22:40:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+  
+    //if(html.classList.contains('light')) {
+    //html.classList.remove('light')
+    //} else { html.classList.add('light')}
+  
+    html.classList.toggle("light")
+    const img = document.querySelector("#profile img")
+    if (html.classList.contains("light")) {
+      img.setAttribute("src", "./fotos/kk-avatar-light.jpg")
+    } else {
+      img.setAttribute("src", "./fotos/kk-avatar.jpg")
+    }
+  }
